455c5db29390daca91c0837f38eb27739f991ffd
Konstantin <xreikox@mail.ru>
2019-11-30 18:25:30 +0300

    fixed

--- script.js
+++ script.js
@@ -8,40 +8,14 @@ let money = +prompt( "Ваш бюджет на месяц?" ),
         income: [],
         savings: false
     },
-    expensesKey = prompt( "Введите обязательную статью расходов в этом месяце" ),
-    expensesValues = +prompt( "Во сколько обойдется?" );
+    expensesKey1 = prompt( "Введите обязательную статью расходов в этом месяце" ),
+    expensesValues1 = +prompt( "Во сколько обойдется?" ),
+    expensesKey2 = prompt( "Введите обязательную статью расходов в этом месяце" ),
+    expensesValues2 = +prompt( "Во сколько обойдется?" );
 
-appData.expenses[ expensesKey ] = expensesValues;
+appData.expenses[ expensesKey1 ] = expensesValues1;
+appData.expenses[ expensesKey2 ] = expensesValues2;
 
-let estimate = `Бюджет на 1 день: ${((appData.moneyData - expensesValues)/30).toFixed(2) } руб.`;
+let estimate = `Бюджет на 1 день: ${(appData.moneyData/30).toFixed(2) } руб.`;
 
 alert( estimate );
-
-/*
-1 ) Сколько типов данных существует в JS?
-Простые типы:
-Числа
-Строки
-Логический
-null
-undefined
-Symbol
-
-Объекты:
-Массивы
-Функции
-Регулярные выражения
-Объект Даты
-Ошибки
-Объект
-
-
-2 ) Как вывести информацию в консоль ?
-Команой console.log("То что мы хотим вывести")
-
-3 ) Какая функция операторов || и && ?
-|| - условие для срабатывания правил, 'если одно из значений верно то...'
-&& - добавление какого либа правила к уже существующему, 'выполни это и еще это' 
-/ условие для срабатывания правил, 'если все значения верны то...'
-    
-*/
